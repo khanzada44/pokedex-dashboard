230944606e37245a5bd5ef6f0817252f6cccdb11
khanzada44 <minhamkhanzada4455@gmail.com>
2026-06-08 18:48:24 +0500

    feat(router): add application routing structure with lazy-loaded pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,31 +1,49 @@
 import { Routes } from '@angular/router';
-import { Pokedex } from './pages/pokedex/pokedex';
-import { Trainer } from './pages/trainer/trainer';
-import { BattleLog } from './pages/battle-log/battle-log';
-import { Battle } from './pages/battle/battle';
-import { TeamBuilder } from './pages/team-builder/team-builder';
-
-
 
 export const routes: Routes = [
-    {
-        path: 'pokedex',
-        component: Pokedex,
-    },
-    {
-        path: 'trainer',
-        component: Trainer,
-    },
-    {
-        path: 'battles-log',
-        component: BattleLog,
-    },
-    {
-        path: 'battles',
-        component: Battle,
-    },
-    {
-        path: 'team-builder',
-        component: TeamBuilder,
-    }
+  {
+    path: '',
+    redirectTo: 'pokedex',
+    pathMatch: 'full',
+  },
+
+  {
+    path: 'pokedex',
+    loadComponent: () =>
+      import('./pages/pokedex/pokedex').then(m => m.Pokedex),
+    title: 'Pokedex',
+  },
+
+  {
+    path: 'trainer',
+    loadComponent: () =>
+      import('./pages/trainer/trainer').then(m => m.Trainer),
+    title: 'Trainer',
+  },
+
+  {
+    path: 'battles-log',
+    loadComponent: () =>
+      import('./pages/battle-log/battle-log').then(m => m.BattleLog),
+    title: 'Battle Log',
+  },
+
+  {
+    path: 'battles',
+    loadComponent: () =>
+      import('./pages/battle/battle').then(m => m.Battle),
+    title: 'Battles',
+  },
+
+  {
+    path: 'team-builder',
+    loadComponent: () =>
+      import('./pages/team-builder/team-builder').then(m => m.TeamBuilder),
+    title: 'Team Builder',
+  },
+
+  {
+    path: '**',
+    redirectTo: 'pokedex',
+  },
 ];
